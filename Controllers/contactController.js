66303f31baaf878a2ece5b13a24f2f602ad8ec5f
pb125 <pb9039172018@gmail.com>
2022-07-29 19:43:40 +0530

    hello

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -22,16 +22,13 @@ class contactController{
     static contactinsert =async(req,res)=>{
         //console.log(req.body)
         try{
+            res.header("Access-Control-Allow-Origin", "*")
             const contact = await contactModel.create(req.body)
 
             res.status(200).json({
                 success:true,
                 contact
             })
-            
-
-           
-
         }catch(error){
             console.log(error)
         }
--- routes/web.js
+++ routes/web.js
@@ -6,8 +6,11 @@ const router = express.Router();
 router.get('/getall',BlogController.getall)
 router.post('/bloginsert',BlogController.bloginsert)
 router.get('/blogview/:id',BlogController.blogview)
-
+router.get('/contactall',contactController.contactall)
 router.post('/contactinsert',contactController.contactinsert)
+
+
+
 
 
 
